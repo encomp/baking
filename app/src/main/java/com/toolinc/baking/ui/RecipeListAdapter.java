ad43e7756e3f5545c24f81a5c8667e34083814b6
encomp <edgar.martinez.rico@gmail.com>
2018-12-10 09:47:37 -0800

    Setting up the skeleton

---
+++ app/src/main/java/com/toolinc/baking/ui/RecipeListAdapter.java
@@ -0,0 +1,6 @@
+package com.toolinc.baking.ui;
+
+import android.widget.BaseAdapter;
+
+public class RecipeListAdapter  {
+}
---
+++ app/src/main/java/com/toolinc/baking/ui/RecipeListFragment.java
@@ -0,0 +1,35 @@
+package com.toolinc.baking.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.toolinc.baking.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/** Renders a specific {@link java.util.List} of recipes. */
+public final class RecipeListFragment extends Fragment {
+
+  @BindView(R.id.rv_recipe_list)
+  private RecyclerView rvRecipeList;
+
+  @Nullable
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+
+    final View rootView = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+    ButterKnife.bind(rootView);
+
+    return rootView;
+  }
+}
