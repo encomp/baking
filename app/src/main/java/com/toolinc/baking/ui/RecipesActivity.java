7898c221150171de002e18771ca98bc33c62a2c2
encomp <edgar.martinez.rico@gmail.com>
2018-12-12 13:36:33 -0800

    Refactoring the main activity.

--- app/src/main/java/com/toolinc/baking/ui/RecipesActivity.java
+++ app/src/main/java/com/toolinc/baking/ui/RecipesActivity.java
@@ -1,16 +1,17 @@
 package com.toolinc.baking.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.toolinc.baking.R;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+/** Main activity that displays a {@link com.toolinc.baking.client.model.Recipes}. */
+public final class RecipesActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_recipes);
+  }
 }
